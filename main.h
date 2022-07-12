6781d9d2dba41270a9532ac40dc3798210826872
pacjnr <105055088+pacjnr@users.noreply.github.com>
2022-07-12 21:03:06 +0100

    main.h file created

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _strlen(char *s);
+int _printf(const char *format, ...);
+int __putchar(char c);
+
+#endif
